45bf42101a00aaefd7dc2587bd3f3c5339337c6c
VANSH KAPOOR <vanshkapoor2001@gmail.com>
2023-06-17 19:13:13 +0530

    changes in contract

--- contracts/NFTee.sol
+++ contracts/NFTee.sol
@@ -4,9 +4,7 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFTee is ERC721 {
-
     constructor() ERC721("NFTee", "ITM") {
         _mint(msg.sender, 1);
     }
-
 }
